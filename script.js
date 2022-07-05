65737676285f811c16d31e48a9af91983d25359f
Sailesh Singh <iamsaileshsingh@gmail.com>
2022-07-05 14:36:18 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let arr=[1,2,3];
+// let doubleArr = [];
+
+// const double = (arr) => {
+//     for (i=0; i<arr.length; i++){
+//     doubleArr.push(arr[i]*2);
+// }
+// console.log(doubleArr)
+// }
+//     double(arr);
+
+
+
+const doubleArr = arr.map(i => i*2);
+console.log(doubleArr);
